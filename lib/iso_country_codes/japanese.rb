7cc9f257d1fcf65682bf383424ddac36b7a47b8f
mofumofu3n <mofumofu3n@gmail.com>
2015-12-22 15:14:24 +0900

    generated jp

---
+++ lib/iso_country_codes/japanese.rb
@@ -0,0 +1,753 @@
+# encoding: utf-8
+
+class IsoCountryCodes
+  class Code
+    class ISL < Code #:nodoc:
+      self.name    = %q{アイスランド}
+    end
+    class IRL < Code #:nodoc:
+      self.name    = %q{アイルランド}
+    end
+    class AZE < Code #:nodoc:
+      self.name    = %q{アゼルバイジャン}
+    end
+    class AFG < Code #:nodoc:
+      self.name    = %q{アフガニスタン}
+    end
+    class USA < Code #:nodoc:
+      self.name    = %q{アメリカ合衆国}
+    end
+    class VIR < Code #:nodoc:
+      self.name    = %q{アメリカ領ヴァージン諸島}
+    end
+    class ASM < Code #:nodoc:
+      self.name    = %q{アメリカ領サモア}
+    end
+    class ARE < Code #:nodoc:
+      self.name    = %q{アラブ首長国連邦}
+    end
+    class DZA < Code #:nodoc:
+      self.name    = %q{アルジェリア}
+    end
+    class ARG < Code #:nodoc:
+      self.name    = %q{アルゼンチン}
+    end
+    class ABW < Code #:nodoc:
+      self.name    = %q{アルバ}
+    end
+    class ALB < Code #:nodoc:
+      self.name    = %q{アルバニア}
+    end
+    class ARM < Code #:nodoc:
+      self.name    = %q{アルメニア}
+    end
+    class AIA < Code #:nodoc:
+      self.name    = %q{アンギラ}
+    end
+    class AGO < Code #:nodoc:
+      self.name    = %q{アンゴラ}
+    end
+    class ATG < Code #:nodoc:
+      self.name    = %q{アンティグア・バーブーダ}
+    end
+    class AND < Code #:nodoc:
+      self.name    = %q{アンドラ}
+    end
+    class YEM < Code #:nodoc:
+      self.name    = %q{イエメン}
+    end
+    class GBR < Code #:nodoc:
+      self.name    = %q{イギリス}
+    end
+    class IOT < Code #:nodoc:
+      self.name    = %q{イギリス領インド洋地域}
+    end
+    class VGB < Code #:nodoc:
+      self.name    = %q{イギリス領ヴァージン諸島}
+    end
+    class ISR < Code #:nodoc:
+      self.name    = %q{イスラエル}
+    end
+    class ITA < Code #:nodoc:
+      self.name    = %q{イタリア}
+    end
+    class IRQ < Code #:nodoc:
+      self.name    = %q{イラク}
+    end
+    class IRN < Code #:nodoc:
+      self.name    = %q{イラン・イスラム共和国}
+    end
+    class IND < Code #:nodoc:
+      self.name    = %q{インド}
+    end
+    class IDN < Code #:nodoc:
+      self.name    = %q{インドネシア}
+    end
+    class WLF < Code #:nodoc:
+      self.name    = %q{ウォリス・フツナ}
+    end
+    class UGA < Code #:nodoc:
+      self.name    = %q{ウガンダ}
+    end
+    class UKR < Code #:nodoc:
+      self.name    = %q{ウクライナ}
+    end
+    class UZB < Code #:nodoc:
+      self.name    = %q{ウズベキスタン}
+    end
+    class URY < Code #:nodoc:
+      self.name    = %q{ウルグアイ}
+    end
+    class ECU < Code #:nodoc:
+      self.name    = %q{エクアドル}
+    end
+    class EGY < Code #:nodoc:
+      self.name    = %q{エジプト}
+    end
+    class EST < Code #:nodoc:
+      self.name    = %q{エストニア}
+    end
+    class ETH < Code #:nodoc:
+      self.name    = %q{エチオピア}
+    end
+    class ERI < Code #:nodoc:
+      self.name    = %q{エリトリア}
+    end
+    class SLV < Code #:nodoc:
+      self.name    = %q{エルサルバドル}
+    end
+    class AUS < Code #:nodoc:
+      self.name    = %q{オーストラリア}
+    end
+    class AUT < Code #:nodoc:
+      self.name    = %q{オーストリア}
+    end
+    class ALA < Code #:nodoc:
+      self.name    = %q{オーランド諸島}
+    end
+    class OMN < Code #:nodoc:
+      self.name    = %q{オマーン}
+    end
+    class NLD < Code #:nodoc:
+      self.name    = %q{オランダ}
+    end
+    class GHA < Code #:nodoc:
+      self.name    = %q{ガーナ}
+    end
+    class CPV < Code #:nodoc:
+      self.name    = %q{カーボベルデ}
+    end
+    class GGY < Code #:nodoc:
+      self.name    = %q{ガーンジー}
+    end
+    class GUY < Code #:nodoc:
+      self.name    = %q{ガイアナ}
+    end
+    class KAZ < Code #:nodoc:
+      self.name    = %q{カザフスタン}
+    end
+    class QAT < Code #:nodoc:
+      self.name    = %q{カタール}
+    end
+    class UMI < Code #:nodoc:
+      self.name    = %q{合衆国領有小離島}
+    end
+    class CAN < Code #:nodoc:
+      self.name    = %q{カナダ}
+    end
+    class GAB < Code #:nodoc:
+      self.name    = %q{ガボン}
+    end
+    class CMR < Code #:nodoc:
+      self.name    = %q{カメルーン}
+    end
+    class GMB < Code #:nodoc:
+      self.name    = %q{ガンビア}
+    end
+    class KHM < Code #:nodoc:
+      self.name    = %q{カンボジア}
+    end
+    class MNP < Code #:nodoc:
+      self.name    = %q{北マリアナ諸島}
+    end
+    class GIN < Code #:nodoc:
+      self.name    = %q{ギニア}
+    end
+    class GNB < Code #:nodoc:
+      self.name    = %q{ギニアビサウ}
+    end
+    class CYP < Code #:nodoc:
+      self.name    = %q{キプロス}
+    end
+    class CUB < Code #:nodoc:
+      self.name    = %q{キューバ}
+    end
+    class CUW < Code #:nodoc:
+      self.name    = %q{キュラソー}
+    end
+    class GRC < Code #:nodoc:
+      self.name    = %q{ギリシャ}
+    end
+    class KIR < Code #:nodoc:
+      self.name    = %q{キリバス}
+    end
+    class KGZ < Code #:nodoc:
+      self.name    = %q{キルギス}
+    end
+    class GTM < Code #:nodoc:
+      self.name    = %q{グアテマラ}
+    end
+    class GLP < Code #:nodoc:
+      self.name    = %q{グアドループ}
+    end
+    class GUM < Code #:nodoc:
+      self.name    = %q{グアム}
+    end
+    class KWT < Code #:nodoc:
+      self.name    = %q{クウェート}
+    end
+    class COK < Code #:nodoc:
+      self.name    = %q{クック諸島}
+    end
+    class GRL < Code #:nodoc:
+      self.name    = %q{グリーンランド}
+    end
+    class CXR < Code #:nodoc:
+      self.name    = %q{クリスマス島}
+    end
+    class GEO < Code #:nodoc:
+      self.name    = %q{グルジア}
+    end
+    class GRD < Code #:nodoc:
+      self.name    = %q{グレナダ}
+    end
+    class HRV < Code #:nodoc:
+      self.name    = %q{クロアチア}
+    end
+    class CYM < Code #:nodoc:
+      self.name    = %q{ケイマン諸島}
+    end
+    class KEN < Code #:nodoc:
+      self.name    = %q{ケニア}
+    end
+    class CIV < Code #:nodoc:
+      self.name    = %q{コートジボワール}
+    end
+    class CCK < Code #:nodoc:
+      self.name    = %q{ココス（キーリング）諸島}
+    end
+    class CRI < Code #:nodoc:
+      self.name    = %q{コスタリカ}
+    end
+    class COM < Code #:nodoc:
+      self.name    = %q{コモロ}
+    end
+    class COL < Code #:nodoc:
+      self.name    = %q{コロンビア}
+    end
+    class COG < Code #:nodoc:
+      self.name    = %q{コンゴ共和国}
+    end
+    class COD < Code #:nodoc:
+      self.name    = %q{コンゴ民主共和国}
+    end
+    class SAU < Code #:nodoc:
+      self.name    = %q{サウジアラビア}
+    end
+    class SGS < Code #:nodoc:
+      self.name    = %q{サウスジョージア・サウスサンドウィッチ諸島}
+    end
+    class WSM < Code #:nodoc:
+      self.name    = %q{サモア}
+    end
+    class STP < Code #:nodoc:
+      self.name    = %q{サントメ・プリンシペ}
+    end
+    class BLM < Code #:nodoc:
+      self.name    = %q{サン・バルテルミー}
+    end
+    class ZMB < Code #:nodoc:
+      self.name    = %q{ザンビア}
+    end
+    class SPM < Code #:nodoc:
+      self.name    = %q{サンピエール島・ミクロン島}
+    end
+    class SMR < Code #:nodoc:
+      self.name    = %q{サンマリノ}
+    end
+    class MAF < Code #:nodoc:
+      self.name    = %q{サン・マルタン（フランス領）}
+    end
+    class SLE < Code #:nodoc:
+      self.name    = %q{シエラレオネ}
+    end
+    class DJI < Code #:nodoc:
+      self.name    = %q{ジブチ}
+    end
+    class GIB < Code #:nodoc:
+      self.name    = %q{ジブラルタル}
+    end
+    class JEY < Code #:nodoc:
+      self.name    = %q{ジャージー}
+    end
+    class JAM < Code #:nodoc:
+      self.name    = %q{ジャマイカ}
+    end
+    class SYR < Code #:nodoc:
+      self.name    = %q{シリア・アラブ共和国}
+    end
+    class SGP < Code #:nodoc:
+      self.name    = %q{シンガポール}
+    end
+    class SXM < Code #:nodoc:
+      self.name    = %q{シント・マールテン（オランダ領）}
+    end
+    class ZWE < Code #:nodoc:
+      self.name    = %q{ジンバブエ}
+    end
+    class CHE < Code #:nodoc:
+      self.name    = %q{スイス}
+    end
+    class SWE < Code #:nodoc:
+      self.name    = %q{スウェーデン}
+    end
+    class SDN < Code #:nodoc:
+      self.name    = %q{スーダン}
+    end
+    class SJM < Code #:nodoc:
+      self.name    = %q{スヴァールバル諸島およびヤンマイエン島}
+    end
+    class ESP < Code #:nodoc:
+      self.name    = %q{スペイン}
+    end
+    class SUR < Code #:nodoc:
+      self.name    = %q{スリナム}
+    end
+    class LKA < Code #:nodoc:
+      self.name    = %q{スリランカ}
+    end
+    class SVK < Code #:nodoc:
+      self.name    = %q{スロバキア}
+    end
+    class SVN < Code #:nodoc:
+      self.name    = %q{スロベニア}
+    end
+    class SWZ < Code #:nodoc:
+      self.name    = %q{スワジランド}
+    end
+    class SYC < Code #:nodoc:
+      self.name    = %q{セーシェル}
+    end
+    class GNQ < Code #:nodoc:
+      self.name    = %q{赤道ギニア}
+    end
+    class SEN < Code #:nodoc:
+      self.name    = %q{セネガル}
+    end
+    class SRB < Code #:nodoc:
+      self.name    = %q{セルビア}
+    end
+    class KNA < Code #:nodoc:
+      self.name    = %q{セントクリストファー・ネイビス}
+    end
+    class VCT < Code #:nodoc:
+      self.name    = %q{セントビンセントおよびグレナディーン諸島}
+    end
+    class SHN < Code #:nodoc:
+      self.name    = %q{セントヘレナ・アセンションおよびトリスタンダクーニャ}
+    end
+    class LCA < Code #:nodoc:
+      self.name    = %q{セントルシア}
+    end
+    class SOM < Code #:nodoc:
+      self.name    = %q{ソマリア}
+    end
+    class SLB < Code #:nodoc:
+      self.name    = %q{ソロモン諸島}
+    end
+    class TCA < Code #:nodoc:
+      self.name    = %q{タークス・カイコス諸島}
+    end
+    class THA < Code #:nodoc:
+      self.name    = %q{タイ}
+    end
+    class KOR < Code #:nodoc:
+      self.name    = %q{大韓民国}
+    end
+    class TWN < Code #:nodoc:
+      self.name    = %q{台湾台湾省中華民国}
+    end
+    class TJK < Code #:nodoc:
+      self.name    = %q{タジキスタン}
+    end
+    class TZA < Code #:nodoc:
+      self.name    = %q{タンザニア}
+    end
+    class CZE < Code #:nodoc:
+      self.name    = %q{チェコ}
+    end
+    class TCD < Code #:nodoc:
+      self.name    = %q{チャド}
+    end
+    class CAF < Code #:nodoc:
+      self.name    = %q{中央アフリカ共和国}
+    end
+    class CHN < Code #:nodoc:
+      self.name    = %q{中華人民共和国}
+    end
+    class TUN < Code #:nodoc:
+      self.name    = %q{チュニジア}
+    end
+    class PRK < Code #:nodoc:
+      self.name    = %q{朝鮮民主主義人民共和国}
+    end
+    class CHL < Code #:nodoc:
+      self.name    = %q{チリ}
+    end
+    class TUV < Code #:nodoc:
+      self.name    = %q{ツバル}
+    end
+    class DNK < Code #:nodoc:
+      self.name    = %q{デンマーク}
+    end
+    class DEU < Code #:nodoc:
+      self.name    = %q{ドイツ}
+    end
+    class TGO < Code #:nodoc:
+      self.name    = %q{トーゴ}
+    end
+    class TKL < Code #:nodoc:
+      self.name    = %q{トケラウ}
+    end
+    class DOM < Code #:nodoc:
+      self.name    = %q{ドミニカ共和国}
+    end
+    class DMA < Code #:nodoc:
+      self.name    = %q{ドミニカ国}
+    end
+    class TTO < Code #:nodoc:
+      self.name    = %q{トリニダード・トバゴ}
+    end
+    class TKM < Code #:nodoc:
+      self.name    = %q{トルクメニスタン}
+    end
+    class TUR < Code #:nodoc:
+      self.name    = %q{トルコ}
+    end
+    class TON < Code #:nodoc:
+      self.name    = %q{トンガ}
+    end
+    class NGA < Code #:nodoc:
+      self.name    = %q{ナイジェリア}
+    end
+    class NRU < Code #:nodoc:
+      self.name    = %q{ナウル}
+    end
+    class NAM < Code #:nodoc:
+      self.name    = %q{ナミビア}
+    end
+    class ATA < Code #:nodoc:
+      self.name    = %q{南極}
+    end
+    class NIU < Code #:nodoc:
+      self.name    = %q{ニウエ}
+    end
+    class NIC < Code #:nodoc:
+      self.name    = %q{ニカラグア}
+    end
+    class NER < Code #:nodoc:
+      self.name    = %q{ニジェール}
+    end
+    class JPN < Code #:nodoc:
+      self.name    = %q{日本}
+    end
+    class ESH < Code #:nodoc:
+      self.name    = %q{西サハラ}
+    end
+    class NCL < Code #:nodoc:
+      self.name    = %q{ニューカレドニア}
+    end
+    class NZL < Code #:nodoc:
+      self.name    = %q{ニュージーランド}
+    end
+    class NPL < Code #:nodoc:
+      self.name    = %q{ネパール}
+    end
+    class NFK < Code #:nodoc:
+      self.name    = %q{ノーフォーク島}
+    end
+    class NOR < Code #:nodoc:
+      self.name    = %q{ノルウェー}
+    end
+    class HMD < Code #:nodoc:
+      self.name    = %q{ハード島とマクドナルド諸島}
+    end
+    class BHR < Code #:nodoc:
+      self.name    = %q{バーレーン}
+    end
+    class HTI < Code #:nodoc:
+      self.name    = %q{ハイチ}
+    end
+    class PAK < Code #:nodoc:
+      self.name    = %q{パキスタン}
+    end
+    class VAT < Code #:nodoc:
+      self.name    = %q{バチカン市国}
+    end
+    class PAN < Code #:nodoc:
+      self.name    = %q{パナマ}
+    end
+    class VUT < Code #:nodoc:
+      self.name    = %q{バヌアツ}
+    end
+    class BHS < Code #:nodoc:
+      self.name    = %q{バハマ}
+    end
+    class PNG < Code #:nodoc:
+      self.name    = %q{パプアニューギニア}
+    end
+    class BMU < Code #:nodoc:
+      self.name    = %q{バミューダ}
+    end
+    class PLW < Code #:nodoc:
+      self.name    = %q{パラオ}
+    end
+    class PRY < Code #:nodoc:
+      self.name    = %q{パラグアイ}
+    end
+    class BRB < Code #:nodoc:
+      self.name    = %q{バルバドス}
+    end
+    class PSE < Code #:nodoc:
+      self.name    = %q{パレスチナ}
+    end
+    class HUN < Code #:nodoc:
+      self.name    = %q{ハンガリー}
+    end
+    class BGD < Code #:nodoc:
+      self.name    = %q{バングラデシュ}
+    end
+    class TLS < Code #:nodoc:
+      self.name    = %q{東ティモール}
+    end
+    class PCN < Code #:nodoc:
+      self.name    = %q{ピトケアン}
+    end
+    class FJI < Code #:nodoc:
+      self.name    = %q{フィジー}
+    end
+    class PHL < Code #:nodoc:
+      self.name    = %q{フィリピン}
+    end
+    class FIN < Code #:nodoc:
+      self.name    = %q{フィンランド}
+    end
+    class BTN < Code #:nodoc:
+      self.name    = %q{ブータン}
+    end
+    class BVT < Code #:nodoc:
+      self.name    = %q{ブーベ島}
+    end
+    class PRI < Code #:nodoc:
+      self.name    = %q{プエルトリコ}
+    end
+    class FRO < Code #:nodoc:
+      self.name    = %q{フェロー諸島}
+    end
+    class FLK < Code #:nodoc:
+      self.name    = %q{フォークランド（マルビナス）諸島}
+    end
+    class BRA < Code #:nodoc:
+      self.name    = %q{ブラジル}
+    end
+    class FRA < Code #:nodoc:
+      self.name    = %q{フランス}
+    end
+    class GUF < Code #:nodoc:
+      self.name    = %q{フランス領ギアナ}
+    end
+    class PYF < Code #:nodoc:
+      self.name    = %q{フランス領ポリネシア}
+    end
+    class ATF < Code #:nodoc:
+      self.name    = %q{フランス領南方・南極地域}
+    end
+    class BGR < Code #:nodoc:
+      self.name    = %q{ブルガリア}
+    end
+    class BFA < Code #:nodoc:
+      self.name    = %q{ブルキナファソ}
+    end
+    class BRN < Code #:nodoc:
+      self.name    = %q{ブルネイ・ダルサラーム}
+    end
+    class BDI < Code #:nodoc:
+      self.name    = %q{ブルンジ}
+    end
+    class VNM < Code #:nodoc:
+      self.name    = %q{ベトナム}
+    end
+    class BEN < Code #:nodoc:
+      self.name    = %q{ベナン}
+    end
+    class VEN < Code #:nodoc:
+      self.name    = %q{ベネズエラ・ボリバル共和国}
+    end
+    class BLR < Code #:nodoc:
+      self.name    = %q{ベラルーシ}
+    end
+    class BLZ < Code #:nodoc:
+      self.name    = %q{ベリーズ}
+    end
+    class PER < Code #:nodoc:
+      self.name    = %q{ペルー}
+    end
+    class BEL < Code #:nodoc:
+      self.name    = %q{ベルギー}
+    end
+    class POL < Code #:nodoc:
+      self.name    = %q{ポーランド}
+    end
+    class BIH < Code #:nodoc:
+      self.name    = %q{ボスニア・ヘルツェゴビナ}
+    end
+    class BWA < Code #:nodoc:
+      self.name    = %q{ボツワナ}
+    end
+    class BES < Code #:nodoc:
+      self.name    = %q{ボネール、シント・ユースタティウスおよびサバ}
+    end
+    class BOL < Code #:nodoc:
+      self.name    = %q{ボリビア多民族国}
+    end
+    class PRT < Code #:nodoc:
+      self.name    = %q{ポルトガル}
+    end
+    class HKG < Code #:nodoc:
+      self.name    = %q{香港}
+    end
+    class HND < Code #:nodoc:
+      self.name    = %q{ホンジュラス}
+    end
+    class MHL < Code #:nodoc:
+      self.name    = %q{マーシャル諸島}
+    end
+    class MAC < Code #:nodoc:
+      self.name    = %q{マカオ}
+    end
+    class MKD < Code #:nodoc:
+      self.name    = %q{マケドニア旧ユーゴスラビア共和国}
+    end
+    class MDG < Code #:nodoc:
+      self.name    = %q{マダガスカル}
+    end
+    class MYT < Code #:nodoc:
+      self.name    = %q{マヨット}
+    end
+    class MWI < Code #:nodoc:
+      self.name    = %q{マラウイ}
+    end
+    class MLI < Code #:nodoc:
+      self.name    = %q{マリ}
+    end
+    class MLT < Code #:nodoc:
+      self.name    = %q{マルタ}
+    end
+    class MTQ < Code #:nodoc:
+      self.name    = %q{マルティニーク}
+    end
+    class MYS < Code #:nodoc:
+      self.name    = %q{マレーシア}
+    end
+    class IMN < Code #:nodoc:
+      self.name    = %q{マン島}
+    end
+    class FSM < Code #:nodoc:
+      self.name    = %q{ミクロネシア連邦}
+    end
+    class ZAF < Code #:nodoc:
+      self.name    = %q{南アフリカ}
+    end
+    class SSD < Code #:nodoc:
+      self.name    = %q{南スーダン}
+    end
+    class MMR < Code #:nodoc:
+      self.name    = %q{ミャンマー}
+    end
+    class MEX < Code #:nodoc:
+      self.name    = %q{メキシコ}
+    end
+    class MUS < Code #:nodoc:
+      self.name    = %q{モーリシャス}
+    end
+    class MRT < Code #:nodoc:
+      self.name    = %q{モーリタニア}
+    end
+    class MOZ < Code #:nodoc:
+      self.name    = %q{モザンビーク}
+    end
+    class MCO < Code #:nodoc:
+      self.name    = %q{モナコ}
+    end
+    class MDV < Code #:nodoc:
+      self.name    = %q{モルディブ}
+    end
+    class MDA < Code #:nodoc:
+      self.name    = %q{モルドバ共和国}
+    end
+    class MAR < Code #:nodoc:
+      self.name    = %q{モロッコ}
+    end
+    class MNG < Code #:nodoc:
+      self.name    = %q{モンゴル}
+    end
+    class MNE < Code #:nodoc:
+      self.name    = %q{モンテネグロ}
+    end
+    class MSR < Code #:nodoc:
+      self.name    = %q{モントセラト}
+    end
+    class JOR < Code #:nodoc:
+      self.name    = %q{ヨルダン}
+    end
+    class LAO < Code #:nodoc:
+      self.name    = %q{ラオス人民民主共和国}
+    end
+    class LVA < Code #:nodoc:
+      self.name    = %q{ラトビア}
+    end
+    class LTU < Code #:nodoc:
+      self.name    = %q{リトアニア}
+    end
+    class LBY < Code #:nodoc:
+      self.name    = %q{リビア}
+    end
+    class LIE < Code #:nodoc:
+      self.name    = %q{リヒテンシュタイン}
+    end
+    class LBR < Code #:nodoc:
+      self.name    = %q{リベリア}
+    end
+    class ROU < Code #:nodoc:
+      self.name    = %q{ルーマニア}
+    end
+    class LUX < Code #:nodoc:
+      self.name    = %q{ルクセンブルク}
+    end
+    class RWA < Code #:nodoc:
+      self.name    = %q{ルワンダ}
+    end
+    class LSO < Code #:nodoc:
+      self.name    = %q{レソト}
+    end
+    class LBN < Code #:nodoc:
+      self.name    = %q{レバノン}
+    end
+    class REU < Code #:nodoc:
+      self.name    = %q{レユニオン}
+    end
+    class RUS < Code #:nodoc:
+      self.name    = %q{ロシア連邦}
+    end
+  end # end Code
+end # IsoCountryCodes
